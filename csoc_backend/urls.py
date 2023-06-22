f5656de7f162aab47ff5f71dde2ed059807b5ce9
Ankit Josh <ankitjosh78@protonmail.com>
2023-06-22 23:33:45 +0530

    Setup Django Templating Engine

---
+++ csoc_backend/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('user/', include('user.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
