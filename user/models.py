4f1a573937c3289e32df6409235c61c33cfc5c1f
Ankit Josh <ankitjosh78@protonmail.com>
2023-06-24 03:48:27 +0530

    Create Team Feature

--- user/models.py
+++ user/models.py
@@ -21,7 +21,6 @@ class User(AbstractUser):
     usn = models.CharField(max_length=10)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-
     class Proficiency(models.TextChoices):
         EXPERT = 'EXP', _('Expert')
         BEGINNER = 'BEG', _('Beginner')
@@ -34,4 +33,18 @@ class User(AbstractUser):
         default=Proficiency.BEGINNER,
     )
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
     objects = CustomUserManager()
+
+
+
+class Team(models.Model):
+    name = models.CharField(_(""), max_length=50)
+
+
+# while accepting make sure that accepting from any team is not true
+class Member(models.Model):
+    user = models.ForeignKey("user.User", verbose_name=_(""), null=True, default=None, on_delete=models.SET_DEFAULT)
+    team = models.ForeignKey(Team, verbose_name=_(""), on_delete=models.CASCADE)
+    acceptance_status = models.BooleanField(_(""))
