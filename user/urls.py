156342128c5790c72c7efbb61388905510d2d3e7
codewithkushagra <kushagraagra008@gmail.com>
2023-06-23 18:20:19 +0530

    added all code

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from django.views.generic import TemplateView
+
+app_name = 'user'
 
 urlpatterns = [
-
+    path('register/', TemplateView.as_view(template_name='registration.html'), name='register')
 ]
