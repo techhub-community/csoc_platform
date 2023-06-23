e1863b431a8719faadf63dd3a9b354e490800b08
codewithkushagra <kushagraagra008@gmail.com>
2023-06-24 01:06:34 +0530

    setup authentication

--- user/forms.py
+++ user/forms.py
@@ -6,21 +6,20 @@ from .models import User, Program
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone_number', 'usn', 'program_selected', 'techstack', 'proficiency', 'password1', 'password2')
+        fields = (
+            'email', 
+            'first_name', 
+            'last_name', 
+            'phone_number', 
+            'usn', 
+            'program_selected', 
+            'techstack', 
+            'proficiency', 
+            'password1', 
+            'password2'
+            )
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(
-        widget=forms.TextInput(
-            attrs={
-                "placeholder": "Email",
-                "class": "form-control"
-            }
-        ))
-    password = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "Password",
-                "class": "form-control"
-            }
-        ))
+    email = forms.EmailField(widget=forms.TextInput())
+    password = forms.CharField(widget=forms.PasswordInput())
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserRegisterView, UserLoginView
+from .views import UserRegisterView, UserLoginView, UserLogoutView
 
 app_name = 'user'
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login')
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout')
 ]
