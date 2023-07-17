5eaf7f2d51e9b7f0785c00978c52402be52f0d46
VishSinh <vish.sinh.vs@gmail.com>
2023-07-17 23:23:19 +0530

    Implemented JWT

--- user/apis/urls.py
+++ user/apis/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 app_name = 'user_api'
 
-urlpatterns = []
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
