bce4a4afc7e4c37906be9ec54d694446ce000fc9
codewithkushagra <kushagraagra008@gmail.com>
2023-07-28 23:03:47 +0530

    added ip logging setup

---
+++ csoc_backend/logging/middleware.py
@@ -0,0 +1,20 @@
+from loguru import logger
+
+
+class RequestIPMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip_address = self.get_client_ip(request)
+        logger.info(f"Request from IP: {ip_address}")
+        response = self.get_response(request)
+        return response
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
