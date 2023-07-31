2b0249bf3859583268a920b491aae702489ba7cd
codewithkushagra <kushagraagra008@gmail.com>
2023-07-31 20:33:02 +0530

    updated gunicorn running route

--- compose/production/django/entrypoint.sh
+++ compose/production/django/entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-gunicorn csoc_backend.wsgi:application -w 2 -b 0.0.0.0:8000
+gunicorn csoc_backend.wsgi:application -w 2 -b unix:/app/gunicorn.sock
